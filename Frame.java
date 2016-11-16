0918a80d6134fd79e2610628af78367d53cdc939
James Magallanes <jamesmagallanes@gmail.com>
2016-11-17 02:41:12 +1300

    Create Frame.java

---
+++ Frame.java
@@ -0,0 +1,17 @@
+package gifprogram;
+
+import java.awt.Color;
+
+public class Frame {
+	
+	private Color[][] arr;
+	private int frameHeight; //obtain sizes from
+	private int frameWidth;  //image parsed in/specified
+	
+	public Frame(){
+		
+		arr = new Color[frameWidth][frameHeight];
+	}
+	
+	
+}
